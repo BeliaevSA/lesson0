f9f83d3317673e09af6d805ec4e8eb20487cd7bf
Beliaev Sergei <94629819+BeliaevSA@users.noreply.github.com>
2024-09-05 01:25:37 +0300

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,19 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+if (1.99 > 9.98 and 1000 != 1000.1):
+    print(True)
+else:
+    print(False)
+
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+#4th program
+str4 = '123.456'
+num4 = float(str4) * 10
+result4 = int(num4 % 10)
+print(result4)
