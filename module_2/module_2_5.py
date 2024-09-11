5d14eae1c85f038771f2d375b276469e184cbbf4
Beliaev Sergei <beliaevsa1@gmail.com>
2024-09-11 06:44:10 +0300

    update file module_2_5

--- module_2/module_2_5.py
+++ module_2/module_2_5.py
@@ -1,4 +1,4 @@
-def get_matrix(*, n: int, m: int, value: int):
+def get_matrix(n, m, value):
     matrix = []
     if n <= 0 or m <= 0:
         return matrix
@@ -8,28 +8,11 @@ def get_matrix(*, n: int, m: int, value: int):
             values.append(value)
         matrix.append(values)
 
-    for list_ in matrix:
-        print(*list_)
-
     return matrix
 
 result1 = get_matrix(n=2, m=2, value=10)
-print(result1)
-print('-------------------------')
-
 result2 = get_matrix(n=3, m=5, value=42)
+result3 = get_matrix(n=4, m=2, value=13)
+print(result1)
 print(result2)
-print('-------------------------')
-
-result3 = get_matrix(n=0, m=2, value=13)
 print(result3)
-print('-------------------------')
-
-result4 = get_matrix(n=4, m=-5, value=13)
-print(result4)
-print('-------------------------')
-
-result5 = get_matrix(n=4, m=2, value=13)
-print(result5)
-
-
