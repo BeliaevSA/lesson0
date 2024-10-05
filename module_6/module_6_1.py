e98352325fd7b5d9bc2c071d8965c5a12e72fcad
Beliaev Sergei <beliaevsa1@gmail.com>
2024-10-06 02:01:09 +0300

    added file module_6_3.py

--- module_6/module_6_1.py
+++ module_6/module_6_1.py
@@ -6,6 +6,16 @@ class Animal:
         self.name = name
 
 
+    def eat(self, food):
+        if isinstance(food, Plant):
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
+
+
 class Plant:
     edible = False
 
@@ -14,26 +24,27 @@ class Plant:
 
 
 class Mammal(Animal):
-    def eat(self, food):
-        if isinstance(food, Plant):
-            if food.edible:
-                print(f'{self.name} съел {food.name}')
-                self.fed = True
-            else:
-                print(f'{self.name} не стал есть {food.name}')
-                self.alive = False
+    pass
+    # def eat(self, food):
+    #     if isinstance(food, Plant):
+    #         if food.edible:
+    #             print(f'{self.name} съел {food.name}')
+    #             self.fed = True
+    #         else:
+    #             print(f'{self.name} не стал есть {food.name}')
+    #             self.alive = False
 
 
 class Predator(Animal):
-
-    def eat(self, food):
-        if isinstance(food, Plant):
-            if food.edible:
-                print(f'{self.name} съел {food.name}')
-                self.fed = True
-            else:
-                print(f'{self.name} не стал есть {food.name}')
-                self.alive = False
+    pass
+    # def eat(self, food):
+    #     if isinstance(food, Plant):
+    #         if food.edible:
+    #             print(f'{self.name} съел {food.name}')
+    #             self.fed = True
+    #         else:
+    #             print(f'{self.name} не стал есть {food.name}')
+    #             self.alive = False
 
 
 class Flower(Plant):
---
+++ module_6/module_6_3.py
@@ -0,0 +1,44 @@
+class Eagle:
+    y_distance = 0
+    sound = 'I train, eat, sleep, and repeat'
+
+    def fly(self, dy: int):
+        self.y_distance += dy
+        return self.y_distance
+
+
+class Horse(Eagle):
+    x_distance = 0
+    sound = "Frrr"
+
+    def get_sound(self):
+        if isinstance(self, Pegasus):
+            return super().sound
+        else:
+            return self.sound
+
+    def run(self, dx: int):
+        self.x_distance += dx
+        return self.x_distance
+
+
+class Pegasus(Horse, Eagle):
+    def voice(self):
+        print(super().get_sound())
+
+    def move(self, dx: int, dy: int):
+        (self.run(dx), self.fly(dy))
+
+    def get_pos(self):
+        return (self.x_distance, self.y_distance)
+
+
+p1 = Pegasus()
+
+print(p1.get_pos())
+p1.move(10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+print(p1.get_pos())
+
+p1.voice()
