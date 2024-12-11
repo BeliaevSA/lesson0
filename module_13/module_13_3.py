a243e244035f8971cab66be0859b4761832420d9
Beliaev Sergei <beliaevsa1@gmail.com>
2024-12-11 12:42:13 +0300

    added file module_13_3.py

---
+++ module_13/module_13_3.py
@@ -0,0 +1,28 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+
+from module_13.config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
